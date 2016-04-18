3222a01c4e096168a5a410b1ec3475d9cd3ae812
chukwudubem  nweke <kcnweke@gmail.com>
2016-04-18 01:40:44 -0700

    added in code to connect to firebase
    it did not work firebase was not properly installed will try again
    tomorrow, but this should connect the app to my firebase database

--- craze/index.android.js
+++ craze/index.android.js
@@ -11,12 +11,26 @@ import React, {
   View
 } from 'react-native';
 
+var Firebase = require('firebase');
+
+class FB_test extends Component{
+  consructor(props){
+    //super(props);
+    var My_FB_test = new Firebase('https://intense-inferno-227.firebaseio.com/');
+
+    My_FB_test.set({
+      title: 'hello world',
+      author:'chuck'
+    });
+  }
+}
+
 class craze extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native test !
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
